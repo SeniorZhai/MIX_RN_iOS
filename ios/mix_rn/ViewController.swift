8733cfd49344fc2a45d8776991eb6a3a4dea5d2e
zhaitao <370985116@qq.com>
2018-05-11 13:33:54 +0800

    First commit

---
+++ ios/mix_rn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mix_rn
+//
+//  Created by zhai on 2018/5/11.
+//  Copyright © 2018年 zhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
