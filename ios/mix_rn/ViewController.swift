a02fdd4e44a94b8079a3ef8f70af0ff983eddeb8
zhaitao <370985116@qq.com>
2018-05-11 18:34:32 +0800

    load local

--- ios/mix_rn/ViewController.swift
+++ ios/mix_rn/ViewController.swift
@@ -12,18 +12,19 @@ import React
 class ViewController: UIViewController {
 
     @IBAction func click(_ sender: Any) {
-        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
-        let mockData:NSDictionary = ["name":"react native"]
-        
-        let rootView = RCTRootView(
-            bundleURL: jsCodeLocation,
-            moduleName: "App",
-            initialProperties: mockData as [NSObject : AnyObject],
-            launchOptions: nil
-        )
-        let vc = UIViewController()
-        vc.view = rootView
-        self.present(vc, animated: true, completion: nil)
+        let path = Bundle.main.path(forResource: "main", ofType: "jsbundle")
+        if path != nil {
+            let mockData:NSDictionary = ["name":"react native"]
+            let rootView = RCTRootView(
+                bundleURL:  URL(fileURLWithPath: path!),
+                moduleName: "App",
+                initialProperties: mockData as [NSObject : AnyObject],
+                launchOptions: nil
+            )
+            let vc = UIViewController()
+            vc.view = rootView
+            self.present(vc, animated: true, completion: nil)
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
