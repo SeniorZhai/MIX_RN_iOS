d5d33d7614ddf8fa1fd7b102a015321024cf19dd
zhaitao <370985116@qq.com>
2018-05-16 16:33:18 +0800

    local

--- ios/mix_rn/ViewController.swift
+++ ios/mix_rn/ViewController.swift
@@ -28,7 +28,48 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let documentsUrl:URL =  (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first as URL?)!
+        let destinationFileUrl = documentsUrl.appendingPathComponent("index2.zip")
+        
+        //Create URL to the source file you want to download
+        let fileURL = URL(string: "https://raw.githubusercontent.com/SeniorZhai/MIX_RN/master/static/1/index.zip")
+        
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        let request = URLRequest(url:fileURL!)
+        
+        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+            if let tempLocalUrl = tempLocalUrl, error == nil {
+                // Success
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                    print("Successfully downloaded. Status code: \(statusCode)")
+                }
+                
+                do {
+                    try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+            
+                    
+                        let mockData:NSDictionary = ["name":"react native"]
+                        let rootView = RCTRootView(
+                            bundleURL:  destinationFileUrl,
+                            moduleName: "APP",
+                            initialProperties: mockData as [NSObject : AnyObject],
+                            launchOptions: nil
+                        )
+                        let vc = UIViewController()
+                        vc.view = rootView
+                        self.present(vc, animated: true, completion: nil)
+                    
+                } catch (let writeError) {
+                    print("Error creating a file \(destinationFileUrl) : \(writeError)")
+                }
+                
+            } else {
+                print("Error took place while downloading a file. Error description: %@", error?.localizedDescription);
+            }
+        }
+         task.resume()
     }
 
     override func didReceiveMemoryWarning() {
